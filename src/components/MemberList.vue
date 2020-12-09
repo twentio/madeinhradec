d4776487daaf4719e84bf864ac417cbc4fd6bb2e
kaldaf <jsem@filipkalousek.cz>
2020-12-09 14:31:28 +0100

    edit

--- src/components/MemberList.vue
+++ src/components/MemberList.vue
@@ -1,10 +1,14 @@
 <template>
     <div class="members">
         <div class="container">
+            <h2 class="head-styled">Seznam členů</h2>
             <ul>
-                <li>
+                <li v-for="member in members" v-bind:key="member">
                     <button>
-                        twentio
+                        <h3>
+                            {{member.name}}
+                        </h3>
+                        <img :str="member.logo" :alt="'logo '+member.name"> 
                     </button>
                 </li>
             </ul>
@@ -28,19 +32,77 @@ interface Member {
 
 
 export default class MemberList extends Vue {
-    members: Member[]=[{
-        name:'',
-        logo:'',
-        summary:'',
-        description:'',
-        contactPlace:'',
-        contactEmail:'',
-        images:[],
-    }]
+    public members: Member[]=[
+        {
+            name:'twentio',
+            logo: require('@/assets/logo.svg'),
+            summary:'',
+            description:'',
+            contactPlace:'',
+            contactEmail:'',
+            images:[],
+        },
+         {
+            name:'twentio',
+            logo: require('@/assets/logo.svg'),
+            summary:'',
+            description:'',
+            contactPlace:'',
+            contactEmail:'',
+            images:[],
+        },
+         {
+            name:'twentio',
+            logo: require('@/assets/logo.svg'),
+            summary:'',
+            description:'',
+            contactPlace:'',
+            contactEmail:'',
+            images:[],
+        },
+          {
+            name:'twentio',
+            logo: require('@/assets/logo.svg'),
+            summary:'',
+            description:'',
+            contactPlace:'',
+            contactEmail:'',
+            images:[],
+        },
+    ]
+ 
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
-  
+  .members{
+      margin: 2rem 0;
+      .container{
+          ul{
+              width: 100%;
+              position: relative;
+              list-style: none;
+              display: flex;
+              align-items: center;
+              flex-wrap: wrap;
+              justify-content: space-between;
+              li{
+                  margin: 0.25rem;
+                  width: calc( ( 100% / 3 ) - 0.5rem);
+                  position: relative;
+                  button{
+                      width: 100%;
+                      min-height: 300px;
+                      background: #fff;
+                      outline: none;
+                      border-radius: 0;
+                      border: none;
+                      cursor: pointer;
+                      border: 1px solid #000;
+                  }
+              }
+          }
+      }
+  }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,8 @@
 <template>
   <Menu />
+  <div class="info">
+    <h1>Stránka je aktuálně ve výstavbě</h1>
+  </div>
   <Footer />
 </template>
 
@@ -16,3 +19,15 @@ import Footer from "@/components/Footer.vue"; // @ is an alias to /src
 })
 export default class Home extends Vue {}
 </script>
+
+<style lang="scss" scoped>
+  .info{
+    width: 100%;
+    height: calc( 100vh - 80px - 32px - 126.4px ) ;
+    position: relative;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    
+    }
+</style>
