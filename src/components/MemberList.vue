d2433e1b193df4599ae0d7c8358af5f0fc3b70b1
kaldaf <jsem@filipkalousek.cz>
2020-12-09 13:34:31 +0100

    members

---
+++ src/components/MemberList.vue
@@ -0,0 +1,46 @@
+<template>
+    <div class="members">
+        <div class="container">
+            <ul>
+                <li>
+                    <button>
+                        twentio
+                    </button>
+                </li>
+            </ul>
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+import { Vue } from "vue-class-component";
+
+interface Member {
+    name: string;
+    logo: string;
+    summary: string;
+    description: string;
+    contactPlace: string;
+    contactEmail: string;
+    images: string[];
+}
+
+
+
+export default class MemberList extends Vue {
+    members: Member[]=[{
+        name:'',
+        logo:'',
+        summary:'',
+        description:'',
+        contactPlace:'',
+        contactEmail:'',
+        images:[],
+    }]
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+  
+</style>
--- src/views/Members.vue
+++ src/views/Members.vue
@@ -1,6 +1,6 @@
 <template>
   <Menu />
-  <h1>Členové</h1>
+  <MemberList />
   <Footer />
 </template>
 
@@ -8,11 +8,13 @@
 import { Options, Vue } from "vue-class-component";
 import Menu from "@/components/Menu.vue"; // @ is an alias to /src
 import Footer from "@/components/Footer.vue"; // @ is an alias to /src
+import MemberList from "@/components/MemberList.vue"; // @ is an alias to /src
 
 @Options({
   components: {
     Menu,
-    Footer
+    Footer,
+    MemberList
   },
 })
 export default class Members extends Vue {}
